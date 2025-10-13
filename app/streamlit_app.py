f8c7453793dcba9b1f468ef24ec666b117a26d6e
marcosmicozzi <micozzimarcos@gmail.com>
2025-10-13 19:43:46 +0200

    streamlit to live app

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,11 +4,30 @@ import psycopg
 
 
 
-def get_data():
+def get_data(crypto):
     dbconn = st.secrets["DB_CONN"]
     conn = psycopg.connect(dbconn)
     cur = conn.cursor()
 
     cur.execute('SELECT * FROM btc_data')
     data = cur.fetchall()
-    print(data)
+
+    cur.close()
+    conn.close()
+
+    return data
+
+
+st.badge("Hello Trader", icon="🍉", color="violet")
+
+option = st.selectbox(
+    "Which Crypto would oyu like to know the price for?",
+    ("BTC", "ETH", "SOL"),
+    index=None
+)
+
+st.write("You selected: ", option)
+
+if option != None:
+    data = get_data(option)
+    st.dataframe(data)
