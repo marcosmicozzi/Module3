9bb29602695c3872e530697d6cf0ddd588778c6c
marcosmicozzi <micozzimarcos@gmail.com>
2025-10-14 11:19:08 +0200

    fixing requirements

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st    
 import psycopg 
-
-
-
+import pandas as pd
 
 def get_data(crypto):
     dbconn = st.secrets["DB_CONN"]
@@ -12,22 +10,23 @@ def get_data(crypto):
     cur.execute('SELECT * FROM btc_data')
     data = cur.fetchall()
 
+    conn.commit()
     cur.close()
     conn.close()
 
-    return data
+    return pd.DataFrame(data, columns=["date", "open", "close"])
 
 
-st.badge("Hello Trader", icon="🍉", color="violet")
+# st.badge("Hello Trader", icon="🍉", color="violet")
 
-option = st.selectbox(
-    "Which Crypto would oyu like to know the price for?",
-    ("BTC", "ETH", "SOL"),
-    index=None
-)
+# option = st.selectbox(
+#     "Which Crypto would oyu like to know the price for?",
+#     ("BTC", "ETH", "SOL"),
+#     index=None
+# )
 
-st.write("You selected: ", option)
+# st.write("You selected: ", option)
 
-if option != None:
-    data = get_data(option)
-    st.dataframe(data)
+# if option != None:
+#     data = get_data(option)
+#     st.dataframe(data)
